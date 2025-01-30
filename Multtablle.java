ddbc5db8448a55d91648bfe5943c7bc5ee9bc7a9
DurgaPrasad0720 <mmprasad901@gmail.com>
2025-01-30 20:41:35 +0530

    Add files via upload

---
+++ Multtablle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Multtablle {
+      public static void main(String[] args) {
+          Scanner sc=new Scanner(System.in);{
+            System.out.println("enter a number");
+            int num =sc.nextInt();
+            for(int i=1;i<=10;i++){
+                System.err.println(num+"*"+i+"="+num*i);
+            }
+            sc.close();
+          }
+      }    
+}
